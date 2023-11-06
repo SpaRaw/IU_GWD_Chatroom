b4afc321f7d85c98a5c1b1193f9762524ed79d7a
SpaRaw <91481370+SpaRaw@users.noreply.github.com>
2023-11-06 14:04:45 +0100

    Template für sql verbindung

--- app/src/index.php
+++ app/src/index.php
@@ -3,7 +3,18 @@
 <title> Hallo </title>
 <body>
 <?php
-    echo("Hallo Welt");
+$servername = "sql";
+$username = "root";
+$password = "zitrone";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=user", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
 </body>
 </html>
